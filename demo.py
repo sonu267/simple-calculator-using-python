296387dfb8659fd9131b6af816f158e4d61b71d7
Sonu Raj <65347096+sonu267@users.noreply.github.com>
2024-04-14 16:07:44 +0530

    Add files via upload
    Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!

---
+++ demo.py
@@ -0,0 +1,15 @@
+print("simple calculator \n please give the number in integer")
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+print("sum of first number",num1,"and sum of second number", num2,"is = ", num1+num2)
+print("multiplication of first number",num1,"with second number", num2,"is = ", num1*num2)
+print("subtraction of first number",num1,"with second number", num2,"is = ", num1-num2)
+print("exponintial of first number",num1,"with second number", num2,"is = ", num1**num2)
+print("division of first number",num1,"with second number", num2,"is = ", num1%num2)
+print("remender of first number",num1,"with second number", num2,"is = ", num1+num2)
+
+
+
+
+
+
